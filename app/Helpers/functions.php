57a8a251be665542d83d9c93fff3c48889478a89
Vortex <xvscode@gmail.com>
2020-11-26 16:58:12 +0800

    commit message

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -1,1 +1,15 @@
 <?php
+
+if (!function_exists('dd')) {
+    function dd(...$vars)
+    {
+        if (empty($vars)) {
+            var_dump('no thing to dd');
+        }
+
+        foreach ($vars as $row) {
+            var_dump($row);
+        }
+        die;
+    }
+}
