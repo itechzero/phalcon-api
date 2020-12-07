8144d7680141b14f90fda116e88c604689b8cea2
Vortex <xvscode@gmail.com>
2020-12-07 20:31:56 +0800

    commit message

---
+++ database/migrations/2020_11_24_204343_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username')->unique()->default('')->comment('name');
+            $table->string('email')->unique()->default('')->comment('email');
+            $table->string('password')->default('')->comment('password');
+            $table->tinyInteger('status')->default(0)->comment('status');
+            $table->unsignedInteger('created_at')->default(0)->comment('created_at');
+            $table->unsignedInteger('updated_at')->default(0)->comment('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
