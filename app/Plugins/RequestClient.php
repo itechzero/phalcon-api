20cc7f837ec8388f265e99dc443dd8c7d97ed95d
Vortex <xvscode@gmail.com>
2020-12-19 14:42:10 +0800

    commit message

--- app/Plugins/RequestClient.php
+++ app/Plugins/RequestClient.php
@@ -3,12 +3,41 @@ declare(strict_types=1);
 
 namespace App\Plugins;
 
-use HttpRequest;
+use GuzzleHttp\Client;
+use GuzzleHttp\TransferStats;
 
 class RequestClient
 {
-    public static function httpGet()
+    private $client;
+
+    public function __construct(array $options = [])
     {
-        return HttpRequest::get('',[]);
+        $defaultOptions = [
+            'on_stats' => function (TransferStats $stats) {
+                $request = $stats->getRequest();
+                $uri = $request->getUri();
+                $body = $request->getBody()->__toString();
+                $method = $request->getMethod();
+
+                if ($stats->hasResponse()) {
+                    $response = $stats->getResponse();
+                    $statusCode = $response->getStatusCode();
+                    $response = $response->getBody()->__toString();
+                    $requestTime = $stats->getTransferTime();
+
+//                    Log::info(sprintf("Url:%s, Method:%s, Body:%s Status:%s Response:%s, Time:%s\n", $uri, $method,
+//                        $body, $statusCode, $response, $requestTime));
+                } else {
+                    //Log::info(sprintf("Url:%s, Method:%s, Body:%s Response:null\n", $uri, $method, $body));
+                }
+            },
+            'timeout' => 3
+        ];
+
+        if ($options) {
+            $defaultOptions = array_merge($defaultOptions, $options);
+        }
+
+        $this->client = new Client($defaultOptions);
     }
 }
