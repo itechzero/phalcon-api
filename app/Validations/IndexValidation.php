a7cf90df1b8cb72188a45805bab0583503c34153
Vortex <xvscode@gmail.com>
2020-12-23 17:16:49 +0800

    commit message

--- app/Validations/IndexValidation.php
+++ app/Validations/IndexValidation.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Validations;
 
+use Phalcon\Validation\Validator\Callback;
 use Phalcon\Validation\Validator\PresenceOf;
 
 class IndexValidation extends BaseValidation
@@ -14,6 +15,29 @@ class IndexValidation extends BaseValidation
             new PresenceOf(
                 [
                     'message' => 'The sign is required',
+                ]
+            )
+        );
+
+        $this->add(
+            'ext',
+            new Callback(
+                [
+                    'callback' => function ($data) {
+
+                        if (empty($data['ext'])) {
+                            return false;
+                        }
+
+                        $ext = json_decode($data['ext'], true);
+
+                        if (empty($ext['resid'])) {
+                            return false;
+                        }
+
+                        return true;
+                    },
+                    'message' => 'The ext error'
                 ]
             )
         );
